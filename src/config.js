17a6866e8a8915841134fae7395da6b6eb05a149
mksglu <bm.ksglu@gmail.com>
2021-01-15 02:56:16 +0300

    feat(application) config

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  collectionNames: {
+    todos: process.env.REACT_APP_TODOS_COLLECTION_NAME,
+    users: process.env.REACT_APP_USERS_COLLECTION_NAME,
+  },
+};
