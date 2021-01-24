789aba2de47b251016247ada4bb8ba122a232be9
mksglu <bm.ksglu@gmail.com>
2021-01-25 01:16:32 +0300

    feat(application) refactored

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,4 @@
 export * from './Button';
 export * from './DeleteButton';
 export * from './Input';
-// export { default as Input } from './input';
-// export { default as Todo } from './todo';
-// export { default as Tasks } from './tasks';
-// export { default as CreateTask } from './createTask';
-// export { default as Logout } from './logout';
+export * from './EditInput';
--- src/layouts/Task.js
+++ src/layouts/Task.js
@@ -2,37 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import { _editTodo } from "../actions";
 import { useSelector, useDispatch } from "react-redux";
+import { useOnClickOutside } from "../hooks";
+import { EditInput } from "../components";
 
 const TodoElement = styled.span`
   background: ${(props) => (props.primary ? "palevioletred" : "white")};
   color: ${(props) => (props.primary ? "white" : "black")};
   font-size: 2em;
 `;
-const InputEditElement = styled.input`
-  background: papayawhip;
-  border: none;
-  border-radius: 3px;
-  font-size: 20px;
-  margin-top: 5px;
-  padding-top: 5px;
-`;
-const FormElement = styled.form``;
-
 const Task = ({ todoId, children }) => {
-  const loading = useSelector((state) => {
-    return state.loadingBar.default;
-  });
+  const loading = useSelector((state) => state.loadingBar.default);
   const inputRef = React.useRef(null);
   const [inputVisible, setInputVisible] = React.useState(false);
   const [text, setText] = React.useState(children);
   const dispatch = useDispatch();
-
-  const onClickOutSide = (event) => {
-    if (inputRef.current && !inputRef.current.contains(event.target)) {
-      setInputVisible(false);
-      dispatch(_editTodo(todoId, text));
-    }
-  };
+  useOnClickOutside(inputRef, () => {
+    setInputVisible(false);
+    dispatch(_editTodo(todoId, text));
+  });
   const handleSubmit = (event) => {
     event.preventDefault();
     setInputVisible(false);
@@ -47,24 +34,15 @@ const Task = ({ todoId, children }) => {
   const handleChange = (event) => {
     setText(event.target.value);
   };
-  React.useEffect(() => {
-    if (inputVisible) {
-      document.addEventListener("mousedown", onClickOutSide);
-    }
-    return () => {
-      document.removeEventListener("mousedown", onClickOutSide);
-    };
-  });
   return (
     <React.Fragment>
       {inputVisible ? (
-        <FormElement onSubmit={handleSubmit}>
-          <InputEditElement
-            ref={inputRef}
-            value={text}
-            onChange={handleChange}
-          />
-        </FormElement>
+        <EditInput
+          onSubmit={handleSubmit}
+          inputRef={inputRef}
+          value={text}
+          onChange={handleChange}
+        ></EditInput>
       ) : (
         <TodoElement onClick={handleClick}>{children}</TodoElement>
       )}
