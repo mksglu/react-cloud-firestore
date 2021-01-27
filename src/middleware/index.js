cc2dc7ae59df7ab3c6530ade3191ec35c7bc368e
mksglu <bm.ksglu@gmail.com>
2021-01-27 23:51:25 +0300

    feat(core) logger

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,7 +3,10 @@ import { applyMiddleware } from "redux";
 import logger from "./logger";
 const getEnvironment = process.env.NODE_ENV;
 
-export default applyMiddleware(
-  thunk,
-  logger
-);
+let middleware = [thunk];
+if (getEnvironment === "development") {
+  middleware = [...middleware, logger];
+}
+
+export default applyMiddleware(...middleware);
+
